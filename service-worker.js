4e25f9bc607eb765a3938e9aeb58dbf82d2ec536
madebysteffi <steffiliebtpost@googlemail.com>
2026-09-15 00:59:06 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,88 @@
+const CACHE_NAME = "ferien-countdown-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
+});
+
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames
+            .filter(name => name !== CACHE_NAME)
+            .map(name => caches.delete(name))
+        )
+      )
+  );
+
+  self.clients.claim();
+});
+
+
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestURL =
+    new URL(event.request.url);
+
+  if (
+    requestURL.origin !==
+    self.location.origin
+  ) {
+    return;
+  }
+
+  event.respondWith(
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const copy =
+          response.clone();
+
+        caches
+          .open(CACHE_NAME)
+          .then(cache =>
+            cache.put(
+              event.request,
+              copy
+            )
+          );
+
+        return response;
+
+      })
+
+      .catch(() =>
+        caches
+          .match(event.request)
+          .then(cachedResponse =>
+            cachedResponse ||
+            caches.match("./index.html")
+          )
+      )
+
+  );
+
+});
